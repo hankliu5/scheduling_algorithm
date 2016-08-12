874013513ad42bf100798811c1280e6f7bb46414
hankliu5 <liu19901124@gmail.com>
2016-08-12 15:17:41 -0700

    works well

--- UserInput.rb
+++ UserInput.rb
@@ -1,9 +1,14 @@
 $LOAD_PATH << '.'
 require 'Scheduling'
 
+def welcome
+  puts
+  puts "Enter the integer hours you want to put into the schedule: (use \"e\" to end, \"s\" to show"
+  puts "and \"d\" to delete the last input)"
+end
+
 puts "Welcome to the demo of scheduling algorithm"
-puts "Enter the integer array you want to sort: (use \"e\" to end, \"s\" to show"
-puts "and \"d\" to delete the last input)"
+welcome
 
 input_ary = Array.new
 
@@ -12,9 +17,11 @@ loop do
   case input
   when "s"
     Scheduling.print_current_schedule(input_ary)
+    welcome
   when "d"
     input_ary.delete(input_ary.last)
     Scheduling.print_current_schedule(input_ary)
+    welcome
   when "e"
     break
   else
@@ -39,23 +46,42 @@ Scheduling.print_current_schedule(input_ary)
 
 puts "\nHow many processors do you want to work with?"
 
-num_processor = gets.chomp
-begin
-  number = Integer(input)
-rescue
-  puts "please enter an integer"
-else
-  if number.to_s != input
+num_processor = 0
+loop do
+  num_processor = gets.chomp
+  begin
+    number = Integer(num_processor)
+  rescue
     puts "please enter an integer"
-    next
+  else
+    if number.to_s != num_processor
+      puts "please enter an integer"
+      next
+    end
+    num_processor = num_processor.to_i
+    break
   end
-  input_ary << number
 end
 
 puts "\nNow, choose one method to plan your schedules (enter \"e\" to exit)"
-puts "1. List Scheduling\n2. Longest Processing Time Scheduling\n3. Shortest Processing Time Scheduling"
 
 loop do
+  puts "\n1. List Scheduling\n2. Longest Processing Time Scheduling\n3. Shortest Processing Time Scheduling\n4. \"e\" to exit"
   input = gets.chomp
   case input
   when "1"
+    puts "Working on List Scheduling..."
+    Scheduling.list_scheduling(input_ary, num_processor)
+  when "2"
+    puts "Working on Longest Processing Time Scheduling..."
+    Scheduling.lpt_scheduling(input_ary, num_processor)
+  when "3"
+    puts "Working on Shortest Processing Time Scheduling..."
+    Scheduling.spt_scheduling(input_ary, num_processor)
+  when "e"
+    puts "Exiting..."
+    break
+  else
+    puts "Wrong Input."
+  end
+end
